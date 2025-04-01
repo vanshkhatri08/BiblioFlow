1f68bbc1b6b92ef32835d27508dfea2fae7d992a
vanshkhatri022@gmail.com <vanshkhatri022@gmail.com>
2025-04-01 13:20:01 +0530

    register completed

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { configureStore } from  "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import popupReducer from "./slices/popUpSlice";
@@ -7,21 +6,5 @@ export const store = configureStore ({
     reducer: {
         auth: authReducer,
         popup: popupReducer,
-=======
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
-import popupReducer from "./slices/popUpSlice";
-import userReducer from "./slices/userSlice";
-import borrowReducer from "./slices/borrowSlice";
-import bookReducer from "./slices/bookSlice";
-
-export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        popup: popupReducer,
-        user: userReducer,
-        borrow: borrowReducer,
-        book: bookReducer,
->>>>>>> e438ebd2bbb360a6da3f818d76aa7dcb2f501de6
     },
 });
--- server/database/db.js
+++ server/database/db.js
@@ -5,11 +5,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
     } catch (error) {
-<<<<<<< HEAD
         console.log("error occured", error); 
-=======
-        console.log("error occurred", error); 
->>>>>>> e438ebd2bbb360a6da3f818d76aa7dcb2f501de6
     }
 }
 export default connectDB;
