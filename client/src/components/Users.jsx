25fa81f5fd76b4c5428cd64ca79ad45ff46771ad
Hemant Raawat <rawathemant254@gmail.com>
2025-04-03 11:36:55 +0530

    user.jsx

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -9,7 +9,7 @@ const Users = () => {
     const date = new Date(timeStamp);
     const formatedDate = '${String(date.getDate()).padStart(2, "0")}-${String(date.getMonth() + 1).padStart(2, "0")}-${date.getFullYear())} ';
     const formattedTime = '${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}';
-    const result =  `${formatedDate} ${formattedTime}`;
+    const result =  `${formatDate} ${formattedTime}`;
     return result
     
   };
