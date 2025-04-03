405f76672e66ac80c93755a69e95bc74d995f5ce
Hemant Raawat <rawathemant254@gmail.com>
2025-04-04 00:04:48 +0530

    record half created 6:47:20

--- client/src/popups/ReadBookPopup.jsx
+++ client/src/popups/ReadBookPopup.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
+import { toggleReadBookPopup } from '../store/slices/popUpSlice';
 
 const ReadBookPopup = () => {
+  const dispatch = useDispatch();
   return (
-    <div>
-      
+    <div className="fixed inset-0 bg-balck bg-opacity-50 flex items-center justify-center z-50">
+      <div className="w-11/12 bg-white rounded-lg shadow-lg sm:w-1/2 lg:w-1/3 2xl:w-1/3">
+       <div className=" flex justify-between items-center bg-black text-white px-6 py-4 rounded-t-lg">
+        <h2 className = "text-white text-lg font-bold"  >
+          View book info
+        </h2>>
+        <h2 className = "text-white text-lg font-bold" onClick={() => dispatchEvent(toggleReadBookPopup)} >
+          <button className='' >
+            &times
+          </button>
+          </div>
+        <div className="p-6">
+        <div className="mb-4">
+        <label className="block text-gray-700 font-semibold ">
+        Book Title
+        </label>
+        <p className="border border-gray-300 rounded-lg px-4 py-2 bg-gray-100">
+        { book && book.title}
+        </p>
+        </div>
+        <div className="mb-4">
+        <label className="block text-gray-700 font-semibold ">
+        Author
+        </label>
+        <p className="border border-gray-300 rounded-lg px-4 py-2 bg-gray-100">
+        { book && book.author}
+        </p>
+        </div>
+        <div className="mb-4">
+        <label className="block text-gray-700 font-semibold ">
+        Description
+        </label>
+        <p className="border border-gray-300 rounded-lg px-4 py-2 bg-gray-100">
+        { book && book.description}
+        </p>
+        </div>
+        </div>
+        < div className ="flex justify-end px-4 bg-gray-100 rounded-b-lg">
+          <button className =" px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300 "
+           type ="button"
+            onClick={() => dispatch(toggleReadBookPopup())}
+              ></button>
+            
+          
+            Close
+          </button>
+        </div>
+
+      </div>
     </div>
   )
 }
 
-export default ReadBookPopup
+export default ReadBookPopup ;
--- client/src/popups/RecordBookPopup.jsx
+++ client/src/popups/RecordBookPopup.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import {useDispatch} from "react-redux"
+import {reactBorrowBook} from "../store/slices/borrowSlice"
 const RecordBookPopup = () => {
+  const dispatch = usDispatch()
+  const HandleRecordBook = (e)=>{
+    e.preventDefault();
+    dispatch(recordBorrowBook(email ,bookId));
+
+  };
   return (
-    <div>
-      
+    <div className="fixed inset-0 bg-balck bg-opacity-50 flex items-center justify-center z-50">
+      <div className="w-full bg-white rounded-lg shadow-lg sm:w-1/2 lg:w-1/3 2xl:w-1/3">
+      <div className ="p-6" >
+          <h3 className="text-xl-font-bold mb-4">
+            Record Book
+          </h3>
+          <form onSubmit = {handleRecordBook}>
+          <div className = "mb-4">
+            <label className ="block text-gray-900 font-medium">User Email</label>
+            <input
+             type ="email"
+              value={email}
+               onChange={(e)=> createSerializableStateInvariantMiddleware(e.ytarget.value)} 
+               placeholder ="Borrower's Email"
+               className = "w-full px-4 py-2 border-black rounded-md "
+               />
+
+          </div>
+          <div className="flex-justify-end space-x-4">
+            <button className ="px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300" type ="button" onClick={}>Close</button>
+            <button>Record</button>
+          </div>
+
+          </form>
+
+          }
+      </div>
+      </div>
     </div>
   )
 }
